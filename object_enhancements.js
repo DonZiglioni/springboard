5ee0fd649a0e454235d523c395e8f71a945ed6ca
DonZiglioni <142073365+DonZiglioni@users.noreply.github.com>
2023-10-28 14:16:13 -0400

    Add files via upload

---
+++ object_enhancements.js
@@ -0,0 +1,101 @@
+// *****     Object Enhancements Exercise     *****
+
+// In this exercise, you’ll refactor some ES5 code into ES2015. Write your code in the sections with a comment to “Write an ES2015 Version”.
+
+// Same keys and values
+
+// function createInstructor(firstName, lastName){
+//   return {
+//     firstName: firstName,
+//     lastName: lastName
+//   }
+// }
+
+
+let createInstructor = (firstName, lastName) => {
+    return {
+        firstName,
+        lastName,
+    }
+}
+console.log(createInstructor("Frank", "Azzaro"));
+
+// Computed Property Names
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+//
+// Computed Property Names ES2015
+// /* Write an ES2015 Version */
+
+let instructor = (firstName, number) => {
+    return {
+        firstName,
+        [number]: "That is my favorite!"
+    }
+}
+console.log(instructor("Frank", 23))
+
+
+// Object Methods
+
+// var instructor = {
+//   firstName: "Colt",
+//   sayHi: function(){
+//     return "Hi!";
+//   },
+//   sayBye: function(){
+//     return this.firstName + " says bye!";
+//   }
+// }
+//
+// Object Methods ES2015
+// /* Write an ES2015 Version */
+
+let instructor2 = {
+    firstName: "Mario",
+    sayHi() {
+        return "Hi!"
+    },
+    sayBye() {
+        return `${this.firstName} says bye!`
+    }
+}
+console.log(instructor2.sayBye());
+
+
+// createAnimal function
+
+// Write a function which generates an animal object. The function should accepts 3 arguments:
+// species: the species of animal (‘cat’, ‘dog’)
+// verb: a string used to name a function (‘bark’, ‘bleet’)
+// noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+// Use one or more of the object enhancements we’ve covered.
+
+// const d = createAnimal("dog", "bark", "Woooof!")
+// // {species: "dog", bark: ƒ}
+// d.bark()  //"Woooof!"
+
+// const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// // {species: "sheep", bleet: ƒ}
+// s.bleet() //"BAAAAaaaa"
+
+
+let createAnimal = (species, verb, noise) => {
+    return {
+        species,
+        verb,
+        noise,
+        [verb]: function () {
+            return noise
+        }
+    }
+}
+
+let animal = createAnimal("Dog", "bark", "Woof!")
+console.log(animal.bark());
